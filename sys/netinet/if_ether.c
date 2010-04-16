f942202a67c6eaf093580274e5963d4eb94fe745
Bjoern A. Zeeb <bz@freebsd.org>
2010-04-16 11:29:23 -0800

    Move things back to the place they were before the VIMAGE/VNET whirl to minimize diffs to stable/7 and earlier.
    [git-p4: depot-paths = "//depot/user/bz/vimage/src/": change = 176982]

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -82,27 +82,27 @@ SYSCTL_DECL(_net_link_ether);
 SYSCTL_NODE(_net_link_ether, PF_INET, inet, CTLFLAG_RW, 0, "");
 SYSCTL_NODE(_net_link_ether, PF_ARP, arp, CTLFLAG_RW, 0, "");
 
-VNET_DEFINE(int, useloopback) = 1;	/* use loopback interface for
-					 * local traffic */
-
 /* timer values */
 static VNET_DEFINE(int, arpt_keep) = (20*60);	/* once resolved, good for 20
 						 * minutes */
-static VNET_DEFINE(int, arpt_down) = 20;      /* keep incomplete entries for
-					       * 20 seconds */
-static VNET_DEFINE(int, arp_maxtries) = 5;
-static VNET_DEFINE(int, arp_proxyall);
-static VNET_DEFINE(struct arpstat, arpstat);  /* ARP statistics, see if_arp.h */
-
 #define	V_arpt_keep		VNET(arpt_keep)
-#define	V_arpt_down		VNET(arpt_down)
-#define	V_arp_maxtries		VNET(arp_maxtries)
-#define	V_arp_proxyall		VNET(arp_proxyall)
-#define	V_arpstat		VNET(arpstat)
 
 SYSCTL_VNET_INT(_net_link_ether_inet, OID_AUTO, max_age, CTLFLAG_RW,
 	&VNET_NAME(arpt_keep), 0,
 	"ARP entry lifetime in seconds");
+
+static VNET_DEFINE(int, arp_maxtries) = 5;
+VNET_DEFINE(int, useloopback) = 1;	/* use loopback interface for
+					 * local traffic */
+static VNET_DEFINE(int, arp_proxyall) = 0;
+static VNET_DEFINE(int, arpt_down) = 20;      /* keep incomplete entries for
+					       * 20 seconds */
+static VNET_DEFINE(struct arpstat, arpstat);  /* ARP statistics, see if_arp.h */
+
+#define	V_arpt_down		VNET(arpt_down)
+#define	V_arp_maxtries		VNET(arp_maxtries)
+#define	V_arp_proxyall		VNET(arp_proxyall)
+#define	V_arpstat		VNET(arpstat)
 
 SYSCTL_VNET_INT(_net_link_ether_inet, OID_AUTO, maxtries, CTLFLAG_RW,
 	&VNET_NAME(arp_maxtries), 0,
