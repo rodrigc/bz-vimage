a04bb907c003232025890f89354fe99bdd82637b
Bjoern A. Zeeb <bz@freebsd.org>
2010-06-29 01:03:12 -0800

    This is the one and only thing on SI_SUB_NETGRAPH and it is the essential module so make it FIRST rather than 2nd so that noone will think something should come before this.
    Sponsored by: The FreeBSD Foundation
    Sponsored by: CK Software GmbH
    
    [git-p4: depot-paths = "//depot/user/bz/vimage/src/": change = 180316]

--- sys/netgraph/ng_base.c
+++ sys/netgraph/ng_base.c
@@ -3170,7 +3170,7 @@ static moduledata_t netgraph_mod = {
 	ngb_mod_event,
 	(NULL)
 };
-DECLARE_MODULE(netgraph, netgraph_mod, SI_SUB_NETGRAPH, SI_ORDER_MIDDLE);
+DECLARE_MODULE(netgraph, netgraph_mod, SI_SUB_NETGRAPH, SI_ORDER_FIRST);
 SYSCTL_NODE(_net, OID_AUTO, graph, CTLFLAG_RW, 0, "netgraph Family");
 SYSCTL_INT(_net_graph, OID_AUTO, abi_version, CTLFLAG_RD, 0, NG_ABI_VERSION,"");
 SYSCTL_INT(_net_graph, OID_AUTO, msg_version, CTLFLAG_RD, 0, NG_VERSION, "");
