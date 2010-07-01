1a17bd41680a62913db6bd37c3124425442d2a4e
Bjoern A. Zeeb <bz@freebsd.org>
2010-07-01 00:21:33 -0800

    Pseudo interfaces go with SI_SUB_PSEUDO.
    Sponsored by: The FreeBSD Foundation
    Sponsored by: CK Software GmbH
    
    [git-p4: depot-paths = "//depot/user/bz/vimage/src/": change = 180370]

--- sys/contrib/pf/net/if_pfsync.c
+++ sys/contrib/pf/net/if_pfsync.c
@@ -2323,7 +2323,7 @@ static moduledata_t pfsync_mod = {
 
 #define PFSYNC_MODVER 1
 
-DECLARE_MODULE(pfsync, pfsync_mod, SI_SUB_PROTO_IFATTACHDOMAIN, SI_ORDER_THIRD);
+DECLARE_MODULE(pfsync, pfsync_mod, SI_SUB_PSEUDO, SI_ORDER_ANY);
 MODULE_VERSION(pfsync, PFSYNC_MODVER);
 MODULE_DEPEND(pflog, pf, PF_MODVER, PF_MODVER, PF_MODVER);
 #endif /* __FreeBSD__ */
